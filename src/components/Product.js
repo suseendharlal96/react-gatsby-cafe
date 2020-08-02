284b58ca787c7345bfe75bb3be131cbd4b4ce022
unknown <lssuseendharlal@gmail.com>
2020-08-02 14:58:39 +0530

    cart added

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,7 +17,7 @@ const Product = ({ product }) => {
               data-item-name={product.title}
               data-item-price={product.price}
               data-item-image={product.image.fluid.src}
-              data-item-url="https://agape-cafe.netlify.com/"
+              data-item-url="https://agape-cafe.netlify.app/"
             >
               add to cart
             </button>

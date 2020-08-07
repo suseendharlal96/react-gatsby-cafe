c7c0a761738afe07ec7cdea1760d8140453994a5
unknown <lssuseendharlal@gmail.com>
2020-08-07 14:41:52 +0530

    progress bar added

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,14 @@ module.exports = {
         autopop: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
